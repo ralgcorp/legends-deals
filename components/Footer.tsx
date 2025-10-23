6d6e55511c13b04467f0825bb3c87ec15369eeb7
ralgcorp <ralgcorp@gmail.com>
2025-10-22 21:18:08 -0300

    feat: adição do logo Coingecko

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <p className="text-sm text-gray-500 mb-4">
         Os dados de distribuições das DEALs #02 - Gunzilla (node), #06 - Runes
         Terminal, #09 - Aethir (node), #11 - Partísia, <br /> #12 - GPU.net
-        (node), #23 - Matrix One #24 - MetaXseed (node), #25 - ZKasino, não
+        (node), #23 - Matrix One, #24 - MetaXseed (node), #25 - ZKasino, não
         encontram-se na planilha
       </p>
       <p>
